ab7b6015dfa296dbf02b208da5668978b247c00b
0kadak <96250124+0kadak@users.noreply.github.com>
2022-08-28 23:14:16 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<script>
+let worddict=
+['one',
+'two',
+'three',
+'four',
+'five',
+'six',
+'seven']
+for (let i=0; i<worddict.length; i++){
+    worddict.splice(i,1,{id:i, item: worddict[i]})
+}
+export default{
+  data(){
+    return{
+      ngword: 'Click to show NG word',
+      counter: 0,
+      wordlist: worddict
+    }
+  },
+  methods:{
+    changeword(){
+      if (this.wordlist.length>0){
+        let ng = this.wordlist[Math.floor(Math.random()*this.wordlist.length)]
+        this.ngword = ng.item
+        this.wordlist = this.wordlist.filter((t)=> t!==ng)
+        this.counter++
+      }
+      else{
+        this.ngword = 'End of Game\nreload to play again'
+      } 
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <div>NGワードゲーム</div>
+    <div>{{counter}}</div>
+  </header>
+
+  <main>
+    <button @click="changeword">{{ngword}}</button>
+  </main>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content:space-between;
+  font-size: 5vw;
+}
+
+main{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  line-height: 5vh;
+}
+
+button{
+  background-color: white;
+  color: blueviolet;
+  display: block;
+  overflow: hidden;
+  font-size: 10vw;
+  box-sizing: border-box;
+  width: 100vw;
+  height: 75vh;
+}
+</style>
